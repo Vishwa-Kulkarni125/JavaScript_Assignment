f26a5bdc3733fe5cecc2bc89cd8b6792467982f8
Vishwambhar R Kulkarni <42531369+Vishwa-Kulkarni125@users.noreply.github.com>
2018-08-29 12:47:21 +0530

    created welcome.js

---
+++ welcome.js
@@ -0,0 +1,12 @@
+ <html>
+<body>
+<h2>To display a message in console </h2>
+
+<p> Please check the message in console window </p>
+
+<script>
+console.log("Welcome to JavaScript");
+</script>
+
+</body>
+</html>
