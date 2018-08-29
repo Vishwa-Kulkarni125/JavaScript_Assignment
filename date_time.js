9cbdb1b200bb51a950d2f22e5a967810845429af
Vishwambhar R Kulkarni <42531369+Vishwa-Kulkarni125@users.noreply.github.com>
2018-08-29 11:59:52 +0530

    date and time assignment done

---
+++ date_time.js
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<h2>To display current time and date</h2>
+<p id="demo">Time and Date</p>
+
+<button type="button" onclick=document.getElementById("demo").innerHTML=Date()>Click Here!</button>
+
+</body>
+</html>
